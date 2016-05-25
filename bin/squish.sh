f8c40177c6f2eb4c7ede5d805eed2c7c93d11a16
root <root@somnus.grex>
2016-05-25 16:13:43 +0200

    WHARRGARBL

--- bin/squish.sh
+++ bin/squish.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-mksquashfs $0 squished.fs -comp xz -b 131072
+#creates squashfs file for HT-TM05 with correct compression and block size
+#usage: squish.sh /path/to/source/filesystem output.filename
+echo path is $1
+echo filename is $2
+
+mksquashfs $1 $2 -comp xz -b 131072
