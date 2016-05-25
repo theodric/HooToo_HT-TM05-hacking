53ba534de14a99545a4852b70c5daa1713b186bf
Theodric <theodric@users.noreply.github.com>
2016-05-25 15:23:43 +0200

    WHARRGARBL

--- bin/checksum_tool.sh
+++ bin/checksum_tool.sh
@@ -2,9 +2,7 @@
 #theodric 20160525
 #this tool generates the checksum needed by the update script. 
 #this is literally the function that does that directly from said script
-#the checksum is of the entire firmware stub, including the line that contains the expected CRCSUM value.
-#Seems like a chicken-egg problem to me.
-#my brain hurts now
-#halp
+#the checksum is of the entire firmware stub minus the top three lines of the file, the last of which contains the CRCSUM.
+#So you have to assemble your firmware, run this to get the checksum, add the CRCSUM into the start_script.sh, and assemble again.
 
 sed '1,3d' $0|cksum|sed -e 's/ /Z/' -e 's/   /Z/'|cut -dZ -f1
