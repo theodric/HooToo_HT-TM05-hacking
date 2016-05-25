f31b60c93d8dfb36a1a5dd3a407a15b906f40f03
root <root@somnus.grex>
2016-05-25 16:41:30 +0200

    WHARRGARBL

--- bin/checksum_tool.sh
+++ bin/checksum_tool.sh
@@ -4,5 +4,6 @@
 #this is literally the function that does that directly from said script
 #the checksum is of the entire firmware stub minus the top three lines of the file, the last of which contains the CRCSUM.
 #So you have to assemble your firmware, run this to get the checksum, add the CRCSUM into the start_script.sh, and assemble again.
+#<pukes gently into sink>
 
 sed '1,3d' $1|cksum|sed -e 's/ /Z/' -e 's/   /Z/'|cut -dZ -f1
