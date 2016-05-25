6fdd9a5af24bc252b4cdab7c2ad909ac004a465f
Theodric <theodric@users.noreply.github.com>
2016-05-25 14:34:54 +0200

    WHARRGARBL

--- bin/Download-and-split.sh
+++ bin/Download-and-split.sh
@@ -7,14 +7,14 @@ MOUNTDIR=${WD}/mount
 #
 # ZIP_FILENAME is the name of the zipfile we download from hootoo
 # UPDATE_FILENAME is the name of the update file we get from the zip file
-ZIP_FILENAME="HooToo-HT-TM05-2.000.022.zip"
-UPDATE_FILENAME="fw-7620-WiFiDGRJ-HooToo-HT-TM05-2.000.022"
+ARCH_FILENAME="HT-TM05-Support%20Special%20Character-2.000.064.rar"
+UPDATE_FILENAME="fw-7620-WiFiDGRJ-HooToo-HT-TM05-2.000.064"
 
-UPDATE_URL="http://www.hootoo.com/media/downloads/${ZIP_FILENAME}"
+UPDATE_URL="http://www.hootoo.com/media/downloads/${ARCH_FILENAME}"
 
 # Need to get and unzip the ZIP file
-wget -O ./${ZIP_FILENAME} ${UPDATE_URL}
-unzip ./${ZIP_FILENAME}
+wget -O ./${ARCH_FILENAME} ${UPDATE_URL}
+unrar e ./${ARCH_FILENAME}
 
 # resulting UPDATE file is a stubfile, so we need to split it
 # this gets where the stub ends and the initrdup begins
